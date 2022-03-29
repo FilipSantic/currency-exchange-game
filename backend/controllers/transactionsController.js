92d22e9d45a1f1288827a31d41d838412861fb2a
Filip Santic <filip.santic@gmail.com>
2022-03-29 22:17:41 +0200

    Middleware handling

--- backend/controllers/transactionsController.js
+++ backend/controllers/transactionsController.js
@@ -3,6 +3,10 @@ const getTransactions = (req, res) => {
 }
 
 const createTransaction = (req, res) => {
+  if(!req.body.transaction) {
+    res.status(400)
+    throw new Error('Creating new transaction failed')
+  }
   res.status(200).json({ message: 'Create new transaction'})
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Routes
 app.use('/api/transactions', require('./routes/transactionRoutes'))
 app.use('/api/money', require('./routes/moneyRoutes'))
+
+// JSON error handler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
