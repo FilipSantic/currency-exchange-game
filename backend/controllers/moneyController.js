a902c764aa8e8eb146602ae8a4a02e59a85fa0bd
Filip Santic <filip.santic@gmail.com>
2022-04-02 14:06:24 +0200

    Update moneyController

--- backend/controllers/moneyController.js
+++ backend/controllers/moneyController.js
@@ -1,5 +1,33 @@
 const asyncHandler = require("express-async-handler");
 const Money = require("../models/moneyModel");
+const axios = require("axios");
+
+const updateMoney = asyncHandler(async (req, res) => {
+  const moneyFromValue = await getMoneyFrom(req, res);
+  const moneyToValueBefore = await getMoneyTo(req, res);
+  const moneyToNewValue = await convertMoneyTo(req, res);
+  const moneyToValueAfter = moneyToValueBefore + Number(moneyToNewValue);
+
+  const moneyFromKey = Object.keys(Money.schema.tree).filter(function (
+    fromKey
+  ) {
+    return fromKey === req.body.from;
+  });
+
+  const moneyToKey = Object.keys(Money.schema.tree).filter(function (toKey) {
+    return toKey === req.body.to;
+  });
+
+  const updatedMoney = await Money.updateOne(
+    { user: req.user.id },
+    {
+      $set: {
+        [`${moneyFromKey}`]: moneyFromValue,
+        [`${moneyToKey}`]: moneyToValueAfter,
+      },
+    }
+  );
+});
 
 const getMoneyFrom = asyncHandler(async (req, res) => {
   const moneyObject = await Money.find({ user: req.user.id });
@@ -9,28 +37,60 @@ const getMoneyFrom = asyncHandler(async (req, res) => {
     throw new Error("Money object not found");
   }
 
-  const moneyValue = moneyObject[0][req.body.from];
+  const moneyFromValue = moneyObject[0][req.body.from];
 
-  if (moneyValue < req.body.amount) {
+  if (moneyFromValue < req.body.amount) {
     res.status(400);
     throw new Error("Not enough money");
   }
 
-  return moneyValue - req.body.amount;
+  return moneyFromValue - req.body.amount;
 });
 
-const updateMoney = asyncHandler(async (req, res) => {
-  const moneyFromValue = await getMoneyFrom(req, res);
-  const moneyFromKey = Object.keys(Money.schema.tree).filter(function (e) {
-    return e === req.body.from;
+const getMoneyTo = asyncHandler(async (req, res) => {
+  const moneyObject = await Money.find({ user: req.user.id });
+
+  if (!moneyObject) {
+    res.status(400);
+    throw new Error("Money object not found");
+  }
+
+  return moneyObject[0][req.body.to];
+});
+
+const convertMoneyTo = asyncHandler(async (req, res) => {
+  const moneyObject = await Money.find({ user: req.user.id });
+
+  if (!moneyObject) {
+    res.status(400);
+    throw new Error("Money object not found");
+  }
+
+  const moneyToKey = Object.keys(Money.schema.tree).filter(function (toKey) {
+    return toKey === req.body.to;
   });
-  const updatedMoneyFrom = await Money.updateOne(
-    { user: req.user.id },
-    { $set: { [`${moneyFromKey}`]: moneyFromValue } }
-  );
+
+  const moneyFromKey = Object.keys(Money.schema.tree).filter(function (
+    fromKey
+  ) {
+    return fromKey === req.body.from;
+  });
+
+  const apiString = `https://api.getgeoapi.com/v2/currency/convert?api_key=${process.env.CURRENCY_API_KEY}&from=${moneyFromKey}&to=${moneyToKey}&amount=${req.body.amount}&format=json`;
+
+  try {
+    const response = await axios.get(apiString);
+
+    return response.data.rates[moneyToKey.toString().toUpperCase()]
+      .rate_for_amount;
+  } catch (err) {
+    throw new Error("Unexpected error happened");
+  }
 });
 
 module.exports = {
-  getMoneyFrom,
   updateMoney,
+  getMoneyFrom,
+  getMoneyTo,
+  convertMoneyTo,
 };
