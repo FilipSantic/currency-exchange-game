01f8c688e08a1cedb54749f29e474206a769a237
Filip Santic <filip.santic.95@gmail.com>
2022-04-16 12:43:44 +0200

    Small changes to index.css file - fix

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -1,6 +1,6 @@
 function TransactionItem({ transaction }) {
   return (
-    <div className="goal">
+    <div className="transaction">
       <div>{new Date(transaction.createdAt).toLocaleString("hr-HR")}</div>
       <h2>Amount: {transaction.amount}</h2>
       <h2>Base: {transaction.base}</h2>
